b4f37ef47ed706216e2461c5e5600f130c09bb79
DESKTOP-0ELLNA6\Admin <hqthai05@gmail.com>
2025-06-27 00:18:44 +0700

    - Fix missing logo in tab navigation - Multi languages: replace toggle button by combo box

--- assets/js/multi-languages.js
+++ assets/js/multi-languages.js
@@ -1,41 +1,25 @@
-// Hàm để đổi ngôn ngữ và thay đổi hình ảnh cờ
-function toggleLanguage() {
-    const checkbox = document.getElementById('language-switch');
-    const thumbFlag = document.getElementById('thumb-flag');
-    const thumb = document.querySelector('.thumb');
-    const switchElement = document.querySelector('.switch');
 
-    // Kiểm tra trạng thái của checkbox và thay đổi ngôn ngữ + cờ
-    if (checkbox.checked) {
-        switchLanguage('vi');
-        thumbFlag.src = "images/flag/vn-flag.png";  // Cờ VN khi bật
-        thumb.style.backgroundColor = '#4CAF50';  // Màu xanh của toggle
-        switchElement.style.backgroundColor = '#4CAF50';  // Màu xanh của toggle
-    } else {
-        switchLanguage('en');
-        thumbFlag.src = "images/flag/uk-flag.png";  // Cờ Anh khi tắt
-        thumb.style.backgroundColor = '#FFFFFF';  // Màu đỏ của toggle
-        switchElement.style.backgroundColor = '#FFFFFF';  // Màu đỏ của toggle
-    }
+function changeLanguage() {
+    const select = document.getElementById('language-select');
+    const flagSpan = document.getElementById('selected-flag');
+    const flagImg = flagSpan.querySelector('img');
+    const selectedOption = select.options[select.selectedIndex];
+    const lang = selectedOption.value;
+    const flag = selectedOption.getAttribute('data-flag');
+  
+    // Cập nhật flag
+    flagImg.src = flag;
+
+    switchLanguage(lang);
 }
 
 // Khi load trang lần đầu, hiển thị đúng VN và load Tiếng Việt
 document.addEventListener('DOMContentLoaded', function() {
-    const checkbox = document.getElementById('language-switch');
-    checkbox.checked = true; // Bật toggle
-    const thumbFlag = document.getElementById('thumb-flag');
-    const thumb = document.querySelector('.thumb');
-    const switchElement = document.querySelector('.switch');
-
-    thumbFlag.src = "images/flag/vn-flag.png"; // Mặc định cờ VN
-    thumb.style.backgroundColor = '#4CAF50';  // Màu xanh của toggle
-    switchElement.style.backgroundColor = '#4CAF50';  // Màu xanh của toggle
-    switchLanguage('vi'); // Load ngôn ngữ Tiếng Việt khi bắt đầu
+    changeLanguage();
 });
 
 function switchLanguage(language) {
     const elements = document.querySelectorAll('[data-en], [data-vi]');
-
     // Loop through each element and change the text based on the selected language
     elements.forEach(function(element) {
         if (language === 'en') {
